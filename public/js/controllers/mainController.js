2e4a4b2c4bf919afcd99ae2d7248b0185bbca43e
John Wu <john.wu@mail.mcgill.ca>
2016-12-15 21:20:21 -0500

    Completed

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -7,14 +7,34 @@ angular.module('mainController', [])
 
         Orders.get().success(function(data) {
 
-            var total = 0.0;
+            var totalRevenue = 0.0;
+            var totalLineItems = 0;
+            var uniqueCities = [];
+            var uniqueCustomers = [];
+
             for ( var i=0; i<data.orders.length; i++ ) {
-                console.log(data.orders[i].total_price);
-                total += parseFloat(data.orders[i].total_price);
+                // get revenue from order
+                totalRevenue += parseFloat(data.orders[i].total_price);
+                // get quantity of items ordered
+                for ( var j=0; j<data.orders[i].line_items.length; j++ ) {
+                    totalLineItems += data.orders[i].line_items[j].quantity;
+                }
+                // get city of shipping address and add to list if it is unique
+                if ( uniqueCities.indexOf( data.orders[i].shipping_address.city ) < 0 ) {
+                    uniqueCities.push( data.orders[i].shipping_address.city );
+                }
+                // get the customer id and add to list if it is unique
+                if ( uniqueCustomers.indexOf( data.orders[i].customer.id ) < 0 ) {
+                    uniqueCustomers.push( data.orders[i].customer.id );
+                }
+
             }
 
             $scope.orders = data.orders;
-            $scope.totalRevenue = total.toFixed(2);
+            $scope.totalRevenue = totalRevenue.toFixed(2);
+            $scope.totalLineItems = totalLineItems;
+            $scope.uniqueCities = uniqueCities.length;
+            $scope.uniqueCustomers = uniqueCustomers.length;
         });
 
     }]);
